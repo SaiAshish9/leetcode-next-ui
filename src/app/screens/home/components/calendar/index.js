5a2f6ffc259400929a8c54d73d50aaf3cf9e6b10
saiashish9 <saiashish3760@gmail.com>
2023-03-29 02:11:13 +0530

    home updated

--- src/app/screens/home/components/calendar/index.js
+++ src/app/screens/home/components/calendar/index.js
@@ -12,10 +12,22 @@ import {
   TimelineText,
   LabelTextContainer,
   Dot,
+  WeeklyCard,
+  CalendarFoot,
+  LabelValue,
+  RedeemV,
+  RedeemCont,
+  RedeemContText,
+  StyledArrowLeftImg,
+  StyledArrowRightImg
 } from "./styles";
 
 import CalendarImg from "@/assets/calendar.png";
 import CalendarSvgImg from "@/assets/calendar.svg";
+import RedeemImg from "@/assets/redeem.png";
+
+import CalendarMarginLeftSvg from "@/assets/calendarMarginLeft.svg";
+import CalendarMarginRightSvg from "@/assets/calendarMarginRight.svg";
 
 const Calender = () => {
   const weekTags = ["S", "M", "T", "W", "T", "F", "S"];
@@ -29,6 +41,8 @@ const Calender = () => {
         <CalendarCont>
           <StyledDayText>Day 28</StyledDayText>
           <TimelineText>07:10:55 left</TimelineText>
+          <StyledArrowLeftImg src={CalendarMarginLeftSvg} alt="img" />
+          <StyledArrowRightImg src={CalendarMarginRightSvg} alt="img" />
         </CalendarCont>
         <StyledImage src={CalendarSvgImg} alt="img" />
         <StyledImage img src={CalendarImg} alt="img" />
@@ -39,12 +53,21 @@ const Calender = () => {
         </TagsCont>
         <LabelsCont>
           {keys.map((date, k) => (
-            <LabelTextContainer key={date}>
-              <LabelText>{date}</LabelText>
-              {k > 2 && k < 34 && <Dot />}
+            <LabelTextContainer highlighted={date == 28} key={date}>
+              <LabelText highlighted={date == 28}>{date}</LabelText>
+              {k > 2 && k < 30 && <Dot />}
             </LabelTextContainer>
           ))}
         </LabelsCont>
+        <WeeklyCard />
+        <CalendarFoot>
+          <RedeemCont>
+            <RedeemV src={RedeemImg} alt="img" />
+            <RedeemContText>0</RedeemContText>
+            <LabelValue tag>Redeem</LabelValue>
+          </RedeemCont>
+          <LabelValue>Rules</LabelValue>
+        </CalendarFoot>
       </Card>
     </Container>
   );
--- src/app/screens/home/components/calendar/styles.js
+++ src/app/screens/home/components/calendar/styles.js
@@ -22,7 +22,7 @@ export const StyledImage = styled(Image)`
   height: 70px;
   width: fit-content;
   position: absolute;
-  right: 1rem;
+  right: 1.2rem;
   margin: 0px;
   padding: 0px;
   cursor: pointer;
@@ -30,7 +30,7 @@ export const StyledImage = styled(Image)`
   ${({ img }) =>
     img
       ? css`
-          right: 0.78rem;
+          right: 0.95rem;
           top: -2rem;
           opacity: 0.6;
           &:hover {
@@ -60,7 +60,7 @@ export const TimelineText = styled.p`
 `;
 
 export const TagsCont = styled.div`
-  margin: 1rem 0px 0.5rem;
+  margin: 0.5rem 0px 0.5rem;
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -86,20 +86,39 @@ export const LabelsCont = styled.div`
 
 export const LabelText = styled.div`
   color: #eff2f699;
-  width: 2rem;
   font-size: 0.75rem;
   display: flex;
   align-items: center;
   justify-content: center;
+  position: relative;
+  ${({ highlighted }) =>
+    highlighted &&
+    css`
+      color: #fff;
+      border-radius: 50%;
+      top: 0.4rem;
+      font-weight: 500;
+    `};
 `;
 
 export const LabelTextContainer = styled.div`
   display: flex;
   align-items: center;
   flex-direction: column;
-  justify-content: center;
+  justify-content: flex-start;
+  width: 2rem;
   height: 2rem;
+  margin-top: 0.24rem;
   cursor: pointer;
+  position: relative;
+  ${({ highlighted }) =>
+    highlighted &&
+    css`
+      color: #fff;
+      background-color: rgb(44 187 93);
+      border-radius: 50%;
+      bottom: 0.4rem;
+    `};
 `;
 
 export const Dot = styled.div`
@@ -108,4 +127,69 @@ export const Dot = styled.div`
   width: 0.25rem;
   border-radius: 50%;
   margin-top: 0.1rem;
+`;
+
+export const WeeklyCard = styled.div`
+  background-image: url("/bg.svg");
+  width: 100%;
+  height: 5rem;
+  background-position: 50%;
+  background-size: cover;
+  background-repeat: no-repeat;
+  padding-left: 0.625rem;
+  padding-right: 0.625rem;
+  border-radius: 0.5rem;
+  margin-top: 0.25rem;
+`;
+
+export const CalendarFoot = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 1rem;
+`;
+
+export const LabelValue = styled.p`
+  font-size: 12px;
+  ${({ tag }) =>
+    tag
+      ? css`
+          color: rgb(44 187 93);
+        `
+      : css`
+          color: #eff2f699;
+        `}
+`;
+
+export const RedeemV = styled(Image)`
+  height: 18px;
+  width: 18px;
+`;
+
+export const RedeemCont = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+`;
+
+export const RedeemContText = styled.div`
+  color: #eff1f6bf;
+  margin-left: 0.5rem;
+  margin-right: 0.5rem;
+  font-weight: 500;
+  font-size: 12px;
+`;
+
+export const StyledArrowLeftImg = styled(Image)`
+  height: 14px;
+  width: 14px;
+  margin-left: 21%;
+`;
+
+export const StyledArrowRightImg = styled(Image)`
+  height: 14px;
+  width: 14px;
+  position: absolute;
+  right: 0.5rem;
+  top: 1.3rem;
 `;
